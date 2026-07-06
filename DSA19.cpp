11a2b9b1bc26b5af2021e393c1eabf41d18f3086
Debopam_Coding <debopamsarkar033@gmail.com>
2026-07-06 22:22:38 +0530

    DSA19.cpp

---
+++ DSA19.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int a, b;
+    char op;
+    cout<<"The value of a is: "<<endl;
+    cin>>a;
+    cout<<"The value of b is: "<<endl;
+    cin>>b;
+    cout<<"The operation is: "<<endl;
+    cin>>op;
+    switch(op) {
+        case('+'): cout<<"The value os addition is: "<<(a+b)<<endl;
+                   break;
+        case('-'): cout<<"The value of substraction is: "<<(a-b)<<endl;
+                   break;
+        case('*'): cout<<"The value of multiplication is: "<<(a*b)<<endl;
+                   break;
+        case('/'): cout<<"The value of division is: "<<(a/b)<<endl;
+                   break;  
+        case('%'): cout<<"The value of remainder is: "<<(a%b)<<endl;
+                   break;  
+        default: cout<<"Please enter the correct operation."<<endl;        
+    }
+    cout<<endl;
+}
+/*
+OUTPUT:
+The value of a is: 
+3
+The value of b is: 
+2
+The operation is: 
++
+The value os addition is: 5
+*/
